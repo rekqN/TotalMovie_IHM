216a0a24dcaf751710f02bb17aab5c09dc167750
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-05-23 19:52:19 +0100

    Ja procura direito

--- src/app/pesquisa/pesquisa.page.ts
+++ src/app/pesquisa/pesquisa.page.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
-interface Movies {
-  [key: string]: {
-    id: string;
-    title_year: string;
-    imdb_rating: string;
-    genre: string;
-    duration: string;
-    synopsis: string;
-    channel: string;
-    img: string;
-    big_img: string;
-    video: string;
-  };
+interface Movie {
+  id: string;
+  title_year: string;
+  imdb_rating: string;
+  genre: string;
+  duration: string;
+  synopsis: string;
+  channel: string;
+  img: string;
+  big_img: string;
+  video: string;
 }
 
 @Component({
@@ -22,14 +19,43 @@ interface Movies {
   styleUrls: ['./pesquisa.page.scss'],
 })
 export class PesquisaPage implements OnInit {
-  public dataMovies: Movies = {};
+  public dataMovies: Movie[] = [];
+  public termoBusca: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.carregarDadosMovies();
   }
-  
-  Pesquisa(){
 
+  carregarDadosMovies() {
+    fetch('./assets/data/movies.json')
+      .then((response) => response.json())
+      .then((json) => {
+        this.dataMovies = json;
+        console.log('Dados dos filmes carregados:', this.dataMovies);
+      })
+      .catch((error) => {
+        console.error('Erro ao carregar dados dos filmes:', error);
+      });
+  }
+
+  pesquisar() {
+    if (this.termoBusca) {
+      const termo = this.termoBusca.toLowerCase();
+
+      const filmesEncontrados = this.dataMovies.filter((movie) =>
+        movie.title_year.toLowerCase().includes(termo)
+      );
+
+      if (filmesEncontrados.length > 0) {
+        console.log('Correspondências encontradas! Filmes:', filmesEncontrados);
+        // Faça o que deseja com os filmes encontrados
+      } else {
+        console.log('Nenhum filme encontrado.');
+      }
+    } else {
+      console.log('Digite um termo de busca.');
+    }
   }
 }
