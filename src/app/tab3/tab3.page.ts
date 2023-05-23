762eb4cdbb9a141754ace5d0d4bfe881ef6d9723
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-05-23 22:28:42 +0100

    Já com as informaçõoes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,38 +3,46 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
 interface Movie {
-  id: string;
-  title_year: string;
-  imdb_rating: string;
-  genre: string;
-  duration: string;
-  synopsis: string;
-  channel: string;
-  img: string;
-  big_img: string;
-  video: string;
+id: string;
+title_year: string;
+imdb_rating: string;
+genre: string;
+duration: string;
+synopsis: string;
+channel: string;
+img: string;
+big_img: string;
+video: string;
 }
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+selector: 'app-tab3',
+templateUrl: 'tab3.page.html',
+styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page implements OnInit {
-  public dataMovies: Movie[] = [];
+public dataMovies: Movie[] = [];
 
-  constructor(private router: Router,private route: ActivatedRoute) {}
+constructor(private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    fetch('./assets/dados/movies.json')
-      .then((res) => res.json())
-      .then((json) => {
-        this.dataMovies = json;
-      });
-  }
+ngOnInit() {
+fetch('./assets/dados/movies.json')
+.then((res) => res.json())
+.then((json) => {
+this.dataMovies = json;
+this.shuffleMovies();
+});
+}
 
-  redirecionarFilme(movie: Movie) {
-    // Redirecionar para a página 'filme' com o ID do filme como parâmetro na URL
-    this.router.navigate(['/filme', movie.id]);
-  }
+shuffleMovies() {
+for (let i = this.dataMovies.length - 1; i > 0; i--) {
+const j = Math.floor(Math.random() * (i + 1));
+[this.dataMovies[i], this.dataMovies[j]] = [this.dataMovies[j], this.dataMovies[i]];
+}
+}
+
+redirecionarFilme(movie: Movie) {
+// Redirecionar para a página 'filme' com o ID do filme como parâmetro na URL
+this.router.navigate(['/filme', movie.id]);
+}
 }
