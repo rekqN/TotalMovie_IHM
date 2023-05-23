f06a5afb5e9fc714794daedcd25db040f06fb5ac
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-05-23 22:03:51 +0100

    Maravilhosoooo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+interface Movie {
+  id: string;
+  title_year: string;
+  imdb_rating: string;
+  genre: string;
+  duration: string;
+  synopsis: string;
+  channel: string;
+  img: string;
+  big_img: string;
+  video: string;
+}
 
 @Component({
-  selector: 'app-tab1',
+  selector: 'app-tab3',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  public dataMovies: Movie[] = [];
 
-  constructor() {}
+  constructor(private router: Router,private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    fetch('./assets/dados/movies.json')
+      .then((res) => res.json())
+      .then((json) => {
+        this.dataMovies = json;
+      });
+  }
+
+  redirecionarFilme(movie: Movie) {
+    // Redirecionar para a página 'filme' com o ID do filme como parâmetro na URL
+    this.router.navigate(['/filme', movie.id]);
+  }
 }
+
