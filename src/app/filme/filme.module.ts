d94c89ecde9aaa8cb7e6c87ea157b6c1ed9b33d2
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-03 01:23:41 +0100

    não dá para por trailers ;(

--- src/app/filme/filme.module.ts
+++ src/app/filme/filme.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { SafePipe } from './safe.pipe';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,6 +16,6 @@ import { FilmePage } from './filme.page';
     IonicModule,
     FilmePageRoutingModule
   ],
-  declarations: [FilmePage]
+  declarations: [FilmePage , SafePipe]
 })
 export class FilmePageModule {}
